c9d7434b4a9e134afbfc120da8c158131c697137
최명승 <ayd07@naver.com>
2017-11-13 09:37:16 +0900

    reply 도메인 수정

--- BlueAuction/src/main/java/kr/co/blueauction/reply/domain/Reply.java
+++ BlueAuction/src/main/java/kr/co/blueauction/reply/domain/Reply.java
@@ -6,19 +6,27 @@ public class Reply {
 	private int product_id;
 	private String content;
 	private String regdate;
+	private int groupNo;
+	private int levelNo;
+	private int orderNo;
 	
 	public Reply() {}
 	
 	public Reply(String member_id, int product_id, String content) {
-		this(0, member_id, product_id, content, null);
+		this(0, member_id, product_id, content, null, 0, 0, 0);
 	}
-
-	public Reply(int reply_id, String member_id, int product_id, String content, String regdate) {
+	
+	public Reply(int reply_id, String member_id, int product_id, String content, String regdate, int groupNo,
+			int levelNo, int orderNo) {
+		super();
 		this.reply_id = reply_id;
 		this.member_id = member_id;
 		this.product_id = product_id;
 		this.content = content;
 		this.regdate = regdate;
+		this.groupNo = groupNo;
+		this.levelNo = levelNo;
+		this.orderNo = orderNo;
 	}
 
 	public int getReply_id() {
@@ -61,13 +69,35 @@ public class Reply {
 		this.regdate = regdate;
 	}
 
+	public int getGroupNo() {
+		return groupNo;
+	}
+
+	public void setGroupNo(int groupNo) {
+		this.groupNo = groupNo;
+	}
+
+	public int getLevelNo() {
+		return levelNo;
+	}
+
+	public void setLevelNo(int levelNo) {
+		this.levelNo = levelNo;
+	}
+
+	public int getOrderNo() {
+		return orderNo;
+	}
+
+	public void setOrderNo(int orderNo) {
+		this.orderNo = orderNo;
+	}
+
 	@Override
 	public String toString() {
 		return "Reply [reply_id=" + reply_id + ", member_id=" + member_id + ", product_id=" + product_id + ", content="
-				+ content + ", regdate=" + regdate + "]";
+				+ content + ", regdate=" + regdate + ", groupNo=" + groupNo + ", levelNo=" + levelNo + ", orderNo="
+				+ orderNo + "]";
 	}
-	
-	
-	
-	
+
 }
