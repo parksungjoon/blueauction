81e9d837075c0a11869e0871734ced93d89c1df3
sujin <kwn01037@naver.com>
2017-11-15 14:48:29 +0900

    입찰(bid)관련 서비스, dao

--- BlueAuction/src/main/java/kr/co/blueauction/bid/service/BidService.java
+++ BlueAuction/src/main/java/kr/co/blueauction/bid/service/BidService.java
@@ -5,6 +5,26 @@
  */
 package kr.co.blueauction.bid.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import kr.co.blueauction.bid.domain.Bid;
+
+@Service
 public interface BidService {
-	
+	// bid등록
+	public void create(Bid bid) throws Exception;
+
+	// bid 수정
+	public void update(Bid bid) throws Exception;
+
+	// 상품 id로 조회
+	public List<Bid> readByProductId(int productId) throws Exception;
+
+	// 회원id로 조회
+	public List<Bid> readByMemberId(String memberId) throws Exception;
+
+	// bid 삭제
+	public void delete(int bidId) throws Exception;
 }
