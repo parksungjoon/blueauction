5cad0f385efb21ea6340e8f495c4a13aa3546065
김봉환 <rlaqhdghks444@gmail.com>
2017-11-20 12:18:56 +0900

    마이페이지 오더 리스트

--- BlueAuction/src/main/java/kr/co/blueauction/order/dao/MybatisOrderDao.java
+++ BlueAuction/src/main/java/kr/co/blueauction/order/dao/MybatisOrderDao.java
@@ -1,6 +1,8 @@
 package kr.co.blueauction.order.dao;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.inject.Inject;
 
@@ -40,5 +42,13 @@ public class MybatisOrderDao implements OrdersDao{
 	@Override
 	public void delete(int orderNo) {
 		sqlSession.delete(NAMESPACE+".delete", orderNo);
+	}
+	/** 로그인된 회원의 구매 리스트를 조회 */
+	@Override
+	public List<Orders> orderList(String memberId, String auctionFlag) throws Exception{
+		Map<String, Object> map=new HashMap<String, Object>();
+		map.put("memberId", memberId);
+		map.put("auctionFlag", auctionFlag);
+		return sqlSession.selectList(NAMESPACE+".orderList", map);
 	}
 }
