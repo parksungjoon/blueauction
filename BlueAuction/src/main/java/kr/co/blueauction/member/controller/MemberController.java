16bb05c59a9700319ff978c0a3e42aea089696bd
박성준 <sjpark9767@gmail.com>
2017-11-29 15:55:45 +0900

    쪽지 Restfull로 수정

--- BlueAuction/src/main/java/kr/co/blueauction/member/controller/MemberController.java
+++ BlueAuction/src/main/java/kr/co/blueauction/member/controller/MemberController.java
@@ -20,6 +20,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -308,7 +309,7 @@ public class MemberController {
 	 * @throws Exception
 	 */
 	@RequestMapping(value = "/mypage/note/list", method = RequestMethod.GET)
-	public String noteList(@ModelAttribute("cri") SearchCriteria cri, HttpSession session, Model model)
+	public String noteList(@ModelAttribute("cri") Criteria cri, HttpSession session, Model model)
 			throws Exception {
 		Object member = session.getAttribute("login");
 		Member member1 = (Member) member;
@@ -321,7 +322,7 @@ public class MemberController {
 		List<Note> notelist = noteService.listByCri(cri, memberId);
 		model.addAttribute("list", notelist);
 
-		PageMaker pageMaker = new PageMaker();
+		PageMaker2 pageMaker = new PageMaker2();
 		pageMaker.setCri(cri);
 
 		pageMaker.setTotalCount(noteService.listCountCriteria(cri, memberId));
@@ -331,14 +332,50 @@ public class MemberController {
 	}
 	
 	/**
-	 * 
+	 * @param page
+	 * @param perPageNum
+	 * @param keyword
+	 * @param session
+	 * @param model
+	 * @return
+	 * @throws Exception
+	 * 받은편지함, 보낸편지함 조회시
+	 */
+	@RequestMapping(value = "/mypage/note/list/{page}/{perPageNum}/{keyword}", method = RequestMethod.GET)
+	public String noteListByCri(@PathVariable("page") int page, @PathVariable("perPageNum") int perPageNum, @PathVariable("keyword") String keyword, HttpSession session, Model model)
+			throws Exception {
+		logger.info("크리투스트링: page" +page+"perPageNum:"+perPageNum+"keyword"+keyword );
+		// login 세션을 가저옴
+		Object member = session.getAttribute("login");
+		Member member1 = (Member) member;
+		Criteria cri=new Criteria();
+		cri.setPage(page);
+		cri.setPerPageNum(perPageNum);
+		cri.setKeyword(keyword);
+		// 세션에 저장되어 있는 멤버에서 memberId를 가저옴
+		String memberId = member1.getMemberId();
+		List<Note> notelist = noteService.listByCri(cri, memberId);
+		logger.info("보낸편지함"+notelist.toString());
+		model.addAttribute("list", notelist);
+
+		PageMaker2 pageMaker = new PageMaker2();
+		pageMaker.setCri(cri);
+
+		pageMaker.setTotalCount(noteService.listCountCriteria(cri, memberId));
+		model.addAttribute("pageMaker", pageMaker);
+
+		return "member/noteList";
+	}
+	
+	/**
 	 * @param noteId
 	 * @param model
 	 * @param session
 	 * @return
+	 * 쪽지 읽기
 	 */
-	@RequestMapping(value = "/mypage/note/read", method = RequestMethod.GET)
-	public String noteRead(@RequestParam("noteId") int noteId, Model model, HttpSession session) {
+	@RequestMapping(value = "/mypage/note/read/{noteId}", method = RequestMethod.GET)
+	public String noteRead(@PathVariable("noteId") int noteId, Model model, HttpSession session) {
 		Note note = noteService.readNote(noteId);
 		Object member = session.getAttribute("login");
 		Member member1 = (Member) member;
