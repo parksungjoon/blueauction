b058bebe01c8b12b785284a61369ff32a5a20a3f
김봉환 <rlaqhdghks444@gmail.com>
2017-11-29 12:28:14 +0900

    메소드주석!

--- BlueAuction/src/main/java/kr/co/blueauction/member/controller/MemberController.java
+++ BlueAuction/src/main/java/kr/co/blueauction/member/controller/MemberController.java
@@ -1,6 +1,7 @@
 package kr.co.blueauction.member.controller;
 
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
@@ -24,6 +25,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import kr.co.blueauction.bid.service.BidService;
 import kr.co.blueauction.common.domain.Criteria;
 import kr.co.blueauction.common.domain.PageMaker;
 import kr.co.blueauction.common.domain.PageMaker2;
@@ -34,8 +36,17 @@ import kr.co.blueauction.member.service.MemberService;
 import kr.co.blueauction.note.domain.Note;
 import kr.co.blueauction.note.service.NoteService;
 import kr.co.blueauction.order.service.OrderService;
+import kr.co.blueauction.product.domain.Product;
 import kr.co.blueauction.product.service.ProductService;
 
+/**
+ * 경매 리스트를 위한 ProductController 
+ *
+ * @author 김봉환
+ * @author 박성준
+ * 
+ * @since 2017. 11. 13.
+ */
 @Controller
 @RequestMapping("/member")
 public class MemberController {
@@ -50,28 +61,37 @@ public class MemberController {
 	private OrderService orderService;
 	@Inject
 	private NoteService noteService;
+	@Inject
+	private BidService bidservice;
 
 	
+	/**
+	 * 로그인 페이지로 이동
+	 * @param req HttpServletRequest(로그인세션얻어옴)
+	 * @return 페이지이동
+	 */
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	public String loginGET(@ModelAttribute("dto") LoginDTO dto, HttpServletRequest req) {
-		logger.info("/member/login 실행");
+	public String loginGET(HttpServletRequest req) {
 		HttpSession session = req.getSession();
 		Object obj = session.getAttribute("login");
 
 		if (obj != null) {
-			logger.info("자동로그인할 login세션이있음");
 			return "redirect:/";
 		} else {
-			logger.info("자동로그인할 login세션이없음");
 		}
 		return "/login";
 	}
+	/**
+	 * 로그인 처리 POST
+	 * @param dto 로그인정보
+	 * @param session session
+	 * @param model model
+	 * @throws Exception
+	 */
 	@RequestMapping(value = "/loginPost", method = RequestMethod.POST)
 	public void loginPOST(LoginDTO dto, HttpSession session, Model model) throws Exception {
-		logger.info("member/loginPost 실행");
 		Member vo = service.login(dto);
 		if (vo == null) {
-
 			return;
 		}
 		model.addAttribute("member", vo);
@@ -84,46 +104,72 @@ public class MemberController {
 		}
 	}
 
+	
+	/**
+	 * 로그아웃을위한 GET
+	 * @throws Exception
+	 */
 	@RequestMapping(value = "/logout", method = RequestMethod.GET)
-	public void logout(HttpServletRequest request, HttpServletResponse response, HttpSession session)
-			throws Exception {
-		System.out.println("membercontroller logout 실행");
-
-	
-	
-
+	public void logout() throws Exception {
 	}
 
+	/**
+	 * 마이페이지이동 GET
+	 * @param member
+	 * @param session
+	 * @param model
+	 * @return
+	 */
 	@RequestMapping(value = "/mypage", method = RequestMethod.GET)
 	public String mypageGET(@ModelAttribute("member") Member member, HttpSession session, Model model) {
 		Object obj = session.getAttribute("login");
 		member = (Member) obj;
-
 		model.addAttribute("member", member);
-
-
 		return "/member/mypage";
 	}
 
+	/**
+	 * 메인페이지 GET
+	 * @param member
+	 * @return
+	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String mainGET(@ModelAttribute("member") Member member) {
+	public String mainGET() {
 		return "/examplePage/main";
 	}
 
+	/**
+	 * 회원가입페이지 이동
+	 * @param member
+	 * @param session
+	 */
 	@RequestMapping(value = "/register", method = RequestMethod.GET)
-	public void memberRegistGET(@ModelAttribute("member") Member member, HttpSession session) {
+	public void memberRegistGET() {
 	}
 
+	/**
+	 * 회원가입처리를위한 POST
+	 * @param member
+	 * @param session
+	 * @param request
+	 * @return
+	 * @throws Exception
+	 */
 	@RequestMapping(value = "/register", method = RequestMethod.POST)
 	public String memberRegistPOST(@ModelAttribute("member") Member member, HttpSession session,
 			HttpServletRequest request) throws Exception {
-		logger.info("/member/register  controller  POST 실행");
 		service.insertMember(member);
 		return "redirect:/";
 	}
 
+	/**
+	 * 아이디 중복확인
+	 * @param req HttpServletRequest
+	 * @param res HttpServletResponse
+	 * @throws Exception
+	 */
 	@RequestMapping(value = "/memberCheck")
-	public void checkId(HttpServletRequest req, HttpServletResponse res, ModelMap model) throws Exception {
+	public void checkId(HttpServletRequest req, HttpServletResponse res) throws Exception {
 		PrintWriter out = res.getWriter();
 		try {
 			String paramId = (req.getParameter("memberId") == null) ? "" : String.valueOf(req.getParameter("memberId"));
@@ -138,9 +184,14 @@ public class MemberController {
 		}
 	}
 
+	/**
+	 * 이메일중복확인
+	 * @param req
+	 * @param res
+	 * @throws Exception
+	 */
 	@RequestMapping(value = "/memberEmailCheck")
-	public void checkEmail(HttpServletRequest req, HttpServletResponse res, ModelMap model) throws Exception {
-		System.out.println("checkEmail 실행");
+	public void checkEmail(HttpServletRequest req, HttpServletResponse res) throws Exception {
 		PrintWriter out = res.getWriter();
 		try {
 			String paramemail = (req.getParameter("email") == null) ? "" : String.valueOf(req.getParameter("email"));
@@ -155,6 +206,13 @@ public class MemberController {
 		}
 	}
 
+	/**
+	 * 이메일 인증을위한 POST
+	 * @param req
+	 * @param res
+	 * @param session
+	 * @throws Exception
+	 */
 	@RequestMapping(value = "/emailAuthenCheck", method = RequestMethod.POST)
 	public void checkEmailAuthenCheck(HttpServletRequest req, HttpServletResponse res, HttpSession session)
 			throws Exception {
@@ -162,7 +220,6 @@ public class MemberController {
 		session.removeAttribute("uid");
 		String frommail = "rlaqhdghks444@gmail.com";
 		String title = "BLUE Aution 메일 인증 입니다";
-		String content = "보내지나!!!!!!!!!";
 		String paramemail = (req.getParameter("email") == null) ? "" : String.valueOf(req.getParameter("email"));
 
 		UUID uuid = UUID.randomUUID();
@@ -178,10 +235,15 @@ public class MemberController {
 		messageHelper.setText(uid);
 
 		mailSender.send(message);
-
-		System.out.println("session.getAttribute(\"uid\")" + session.getAttribute("uid"));
 	}
 
+	/**
+	 * UID 확인을위한 POST
+	 * @param req
+	 * @param res
+	 * @param session
+	 * @throws Exception
+	 */
 	@RequestMapping(value = "/uidCheck", method = RequestMethod.POST)
 	public void uidCheck(HttpServletRequest req, HttpServletResponse res, HttpSession session) throws Exception {
 		PrintWriter out = res.getWriter();
@@ -197,19 +259,22 @@ public class MemberController {
 		out.close();
 	}
 
+	/**
+	 * 해당아이디에 중고판매리스트로 이동
+	 * @param cri
+	 * @param session
+	 * @param model
+	 * @return
+	 * @throws Exception
+	 */
 	@RequestMapping(value = "/mypage/goodsmarket", method = RequestMethod.GET)
 	public String goodsmarket(@ModelAttribute("cri") Criteria cri, HttpSession session, Model model)
 			throws Exception {
-		// login 세션을 가저옴
 		Object member = session.getAttribute("login");
-		logger.info("/member/mypage/goodsmarket에서 " + member.toString());
 		Member member1 = (Member) member;
-		// 세션에 저장되어 있는 멤버에서 memberId를 가저옴
 		String memberId = member1.getMemberId();
 		String auctionFlag = "N";
-		// 상품을 받아옴
-		model.addAttribute("products", productService.productSellListCriteria(cri, memberId, auctionFlag));
-		logger.info("String.valueOf(productService.listCountCriteria(cri))"+String.valueOf(productService.listCountCriteria(memberId, auctionFlag)));
+		model.addAttribute("products", productService.productSellListCriteria(cri, memberId, auctionFlag).get("products"));
 		PageMaker2 pageMaker2 = new PageMaker2();
 		pageMaker2.setCri(cri);
 		pageMaker2.setTotalCount(productService.listCountCriteria(memberId,auctionFlag));
@@ -218,41 +283,48 @@ public class MemberController {
 	}
 
 
+	/**
+	 * 해당아이디에 옥션판매리스트로 이동
+	 * @param cri
+	 * @param session
+	 * @param model
+	 * @return
+	 * @throws Exception
+	 */
 	@RequestMapping(value = "/mypage/auctionmarket", method = RequestMethod.GET)
 	public String auctionmarket(@ModelAttribute("cri") Criteria cri, HttpSession session, Model model)
 			throws Exception {
-		// login 세션을 가저옴
 		Object member = session.getAttribute("login");
-		logger.info("/member/mypage/goodsmarket에서 " + member.toString());
 		Member member1 = (Member) member;
-		// 세션에 저장되어 있는 멤버에서 memberId를 가저옴
 		String memberId = member1.getMemberId();
 		String auctionFlag = "Y";
-		// 상품을 받아옴
-		model.addAttribute("products", productService.productSellListCriteria(cri, memberId, auctionFlag));
-		logger.info("String.valueOf(productService.listCountCriteria(cri))"+String.valueOf(productService.listCountCriteria(memberId, auctionFlag)));
+		model.addAttribute("products", productService.productSellListCriteria(cri, memberId, auctionFlag).get("products"));
+		
 		PageMaker2 pageMaker2 = new PageMaker2();
 		pageMaker2.setCri(cri);
 		pageMaker2.setTotalCount(productService.listCountCriteria(memberId, auctionFlag));
 		model.addAttribute("pageMaker", pageMaker2);
-	
 		return "member/auctionmarket";
 	}
 
+	/**
+	 * 해당아이디에 note리스트로 이동
+	 * @param cri
+	 * @param session
+	 * @param model
+	 * @return
+	 * @throws Exception
+	 */
 	@RequestMapping(value = "/mypage/note/list", method = RequestMethod.GET)
 	public String noteList(@ModelAttribute("cri") SearchCriteria cri, HttpSession session, Model model)
 			throws Exception {
-		logger.info("크리투스트링:" + cri.toString());
-		// login 세션을 가저옴
 		Object member = session.getAttribute("login");
-		logger.info("/member/mypage/notelist에서 " + member.toString());
 		Member member1 = (Member) member;
 
 		if (cri.getKeyword() == null) {
 			cri.setKeyword("R");
 		}
 
-		// 세션에 저장되어 있는 멤버에서 memberId를 가저옴
 		String memberId = member1.getMemberId();
 		List<Note> notelist = noteService.listByCri(cri, memberId);
 		model.addAttribute("list", notelist);
@@ -265,16 +337,21 @@ public class MemberController {
 
 		return "member/noteList";
 	}
-
+	
+	/**
+	 * 
+	 * @param noteId
+	 * @param model
+	 * @param session
+	 * @return
+	 */
 	@RequestMapping(value = "/mypage/note/read", method = RequestMethod.GET)
 	public String noteRead(@RequestParam("noteId") int noteId, Model model, HttpSession session) {
 		Note note = noteService.readNote(noteId);
 		Object member = session.getAttribute("login");
 		Member member1 = (Member) member;
 		String memberId = member1.getMemberId();
-		logger.info("읽는 사람" + memberId + "받는사람" + note.getReceiver());
 		if (note.getReceiver().equalsIgnoreCase(memberId)) {
-			logger.info("왜 여기로 안들어오니");
 			noteService.updateReadDate(noteId);
 		}
 		model.addAttribute(note);
@@ -282,6 +359,12 @@ public class MemberController {
 		return "member/noteRead";
 	}
 
+	/**
+	 * 해당아이디에 note 삭제POST
+	 * @param noteId
+	 * @param rttr
+	 * @return
+	 */
 	@RequestMapping(value = "/mypage/note/delete", method = RequestMethod.POST)
 	public String noteDelete(@RequestParam("noteId") int noteId, RedirectAttributes rttr) {
 		noteService.deleteNote(noteId);
@@ -289,5 +372,6 @@ public class MemberController {
 
 		return "redirect:/member/mypage/note/list";
 	}
+
 
 }
