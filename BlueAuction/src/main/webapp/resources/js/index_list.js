94357cddade56ad3097b491e7f03e2ccd1b044b5
jihyun <surinim@naver.com>
2017-11-30 21:12:02 +0900

    메인페이지 입찰 가격 표시 수정

--- BlueAuction/src/main/webapp/resources/js/index_list.js
+++ BlueAuction/src/main/webapp/resources/js/index_list.js
@@ -51,9 +51,9 @@
       	  	  		html +="             <p class=''><strong>Current Price</strong></p>";
       	  	  		
       	      	  	if(data[i].bidprice != 0){
-      	            	html +="<h6>" + setComma(data[i].price) + "원</h6>";
+      	            	html +="<h6>" + setComma(data[i].bidprice) + "원</h6>";
       	            }else{
-      	            	html +="<h6 class='jjh-notSuccess'>" + setComma(data[i].price) + "원</h6>";
+      	            	html +="<h6 class='jjh-notSuccess'>" + setComma(data[i].basicprice) + "원</h6>";
       	            }
       	      	  	
       	  	  		html +="           </div>";
